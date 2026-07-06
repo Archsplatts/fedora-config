d70462c789864148642b4de152638239cd268179
Archsplatts <bloodsplatts@gmail.com>
2026-07-06 10:20:39 +0200

    Update autostart.sh

--- config/mango/autostart.sh
+++ config/mango/autostart.sh
@@ -2,7 +2,7 @@
 
 set +e
 
-/usr/libexec/xdg-desktop-portal-wlr >/dev/null 2>&1 &
+systemctl --user start xdg-desktop-portal.service --ignore-dependencies &
 
 ~/.config/waybar/scripts/dnf-status &
 
@@ -14,10 +14,8 @@ swaybg -i ~/Images/Wallpapers/crane.png >/dev/null 2>&1 &
 
 waybar -c ~/.config/mango/waybar/config.jsonc -s ~/.config/mango/waybar/style.css >/dev/null 2>&1 &
 
-# Keep clipboard content after app closes
 wl-clip-persist --clipboard regular --reconnect-tries 0 &
 
-# Watch clipboard and store history
 wl-paste --type text --watch cliphist store &
 
 nm-applet --indicator >/dev/null 2>&1 &
